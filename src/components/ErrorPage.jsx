c184b741dfa114f2456fc9bb8a2095ce152495e9
thetamimahmed <wwwtamim000@gmail.com>
2023-05-04 01:41:40 +0600

    added error page content

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './Header';
+import {Link} from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
         <div>
             <Header></Header>
-            <h1>Error Page</h1>
+            <div className='error-banner d-flex align-items-center justify-content-center '>
+                <div className='text-center'>
+                    <h1 className='primary-color' style={{fontSize:'120px'}}>404</h1>
+                    <h3 className='primary-color'>Sorry Page Was Not Found</h3>
+                    <button className='btn btn-primary custom-btn my-3'>
+                        <Link className='text-decoration-none text-white' to="/">Back to home</Link>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
