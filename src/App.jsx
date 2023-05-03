399175b1262e52cd4b34c7536b9f572d7a079067
thetamimahmed <wwwtamim000@gmail.com>
2023-05-03 17:51:50 +0600

    file setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center mt-2'>Default</h1>
+    </div>
+  );
+};
+
+export default App;
