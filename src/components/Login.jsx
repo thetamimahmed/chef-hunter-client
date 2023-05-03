4e95989e04a18099b5859d1861a6d2e25df6fd5e
thetamimahmed <wwwtamim000@gmail.com>
2023-05-03 19:27:11 +0600

    added login page style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <h1>Login</h1>
+            <h3 className='text-center primary-color my-3'>Please Login Your Account</h3>
+            <Form className='w-50 mx-auto'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+
+                <Button className='custom-btn' variant="primary" type="submit">
+                    Submit
+                </Button>
+                <p className='mt-2'>New in Italian Chef Hunter? <span><Link className='primary-color' to={"/register"}>Register</Link></span></p>
+            </Form>
         </div>
     );
 };
