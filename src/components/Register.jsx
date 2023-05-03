0920997e3e85ce4ae7b6acfea813c699ee73c20f
thetamimahmed <wwwtamim000@gmail.com>
2023-05-03 19:22:59 +0600

    added register form style

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h1>Register</h1>
+            <h3 className='text-center my-3 primary-color'>Please Register Your Account</h3>
+            <Form className='w-50 mx-auto'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter name" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password"  required/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Photo url</Form.Label>
+                    <Form.Control type="text" name='photourl' placeholder="Enter photo url" required />
+                </Form.Group>
+                <Button className='custom-btn' variant="primary" type="submit">
+                    Submit
+                </Button>
+                <p className='mt-2'>Already have an account? <span><Link className='primary-color' to={"/login"}>Log In</Link></span></p>
+            </Form>
         </div>
     );
 };
